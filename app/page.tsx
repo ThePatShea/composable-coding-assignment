d9e63eb03cb0de71f02b7b2a5c33adabbe935322
Patrick Shea <1574933+ThePatShea@users.noreply.github.com>
2024-02-28 16:41:23 -0500

    Implemented Roboto font and started top section

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
-      Composable
+    <main className="flex justify-center pt-12">
+      <div className="p-6 rounded-3xl border border-white border-opacity-10">
+        <h1 className="text-white text-2xl font-medium leading-none mb-2">Assignment block explorer</h1>
+        <h6 className="text-white text-opacity-60 text-sm font-medium leading-none">Assignment block explorer</h6>
+      </div>
     </main>
   );
 }
