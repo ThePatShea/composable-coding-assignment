6b66ceee361cadb4b124efabab5397038665f849
Patrick Shea <1574933+ThePatShea@users.noreply.github.com>
2024-02-28 17:34:48 -0500

    Styling the search input

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ export default function Home() {
     <main className="flex justify-center pt-12 font-roboto-light">
       <div className="flex flex-col w-[800px]">
         <div className="p-6 rounded-3xl border border-white border-opacity-10 mb-10">
-          <h1 className="text-white text-2xl font-medium leading-none mb-2">Assignment block explorer</h1>
+          <h1 className="text-white text-2xl font-medium leading-none mb-3">Assignment block explorer</h1>
           <h6 className="text-white text-opacity-60 text-sm font-medium leading-none">Check list of blocks and detailed view.</h6>
         </div>
         <input
           type="text"
-          className="rounded-2xl bg-white bg-white-02 px-4 py-5 text-white text-opacity-60"
+          className="rounded-2xl bg-white bg-white-02 hover:bg-white-05 px-4 py-5 text-white text-sm text-opacity-60 placeholder-gray-50 placeholder-opacity-60 hover:placeholder-opacity-100"
           placeholder="Search for transactions, blocks, accounts"
         />
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,10 +14,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        'roboto-light': ['"Roboto Light"', 'sans-serif'], // Add Gilroy to the font family
+        'roboto-light': ['"Roboto Light"', 'sans-serif'],
       },
       colors: {
         'white-02': 'rgba(255, 255, 255, 0.02)',
+        'white-05': 'rgba(255, 255, 255, 0.05)',
       },
     },
   },
