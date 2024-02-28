721fd958f97f48caf464de61c810817865beb535
Patrick Shea <1574933+ThePatShea@users.noreply.github.com>
2024-02-28 17:11:16 -0500

    Started implementing search bar

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex justify-center pt-12">
-      <div className="p-6 rounded-3xl border border-white border-opacity-10">
-        <h1 className="text-white text-2xl font-medium leading-none mb-2">Assignment block explorer</h1>
-        <h6 className="text-white text-opacity-60 text-sm font-medium leading-none">Assignment block explorer</h6>
+    <main className="flex justify-center pt-12 font-roboto-light">
+      <div className="flex flex-col w-[800px]">
+        <div className="p-6 rounded-3xl border border-white border-opacity-10 mb-10">
+          <h1 className="text-white text-2xl font-medium leading-none mb-2">Assignment block explorer</h1>
+          <h6 className="text-white text-opacity-60 text-sm font-medium leading-none">Check list of blocks and detailed view.</h6>
+        </div>
+        <input
+          type="text"
+          className="rounded-2xl bg-white bg-white-02 px-4 py-5 text-white text-opacity-60"
+          placeholder="Search for transactions, blocks, accounts"
+        />
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        'roboto-light': ['"Roboto Light"', 'sans-serif'], // Add Gilroy to the font family
+      },
+      colors: {
+        'white-02': 'rgba(255, 255, 255, 0.02)',
+      },
     },
   },
   plugins: [],
