ab216bd61e7455b22ff07e1c4f535305098a45ad
Patrick Shea <1574933+ThePatShea@users.noreply.github.com>
2024-02-29 18:33:22 -0500

    Made the back button clickable

--- components/InfoBox.tsx
+++ components/InfoBox.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 interface InfoBoxProps {
   title: string;
   subtitle: string;
@@ -9,9 +7,15 @@ interface InfoBoxProps {
 const InfoBox: React.FC<InfoBoxProps> = ({ title, subtitle, colSpan }) => {
   return (
     <div
-      className={`col-span-${String(
-        colSpan
-      )} bg-white-opacity-02 px-1 py-6 rounded-3xl shadow-lg`}
+      className={`${
+        colSpan === 1
+          ? "col-span-1"
+          : colSpan === 2
+          ? "col-span-2"
+          : colSpan === 3
+          ? "col-span-3"
+          : "col-span-4"
+      } bg-white-opacity-02 px-1 py-6 rounded-3xl shadow-lg`}
     >
       <h6 className="text-white text-center text-opacity-60 text-xs leading-none mb-3">
         {title}
--- pages/block/[slot].tsx
+++ pages/block/[slot].tsx
@@ -9,6 +9,7 @@ import { selectBlock } from "@/reducers/blockSlice";
 import getRelativeTime from "@/helpers/getRelativeTime";
 import formatAsUsd from "@/helpers/formatAsUsd";
 
+import BackIcon from "@/components/SvgIcon/BackIcon";
 import PageHeading from "@/components/PageHeading";
 import InfoBox from "@/components/InfoBox";
 
@@ -35,6 +36,11 @@ export default function Block() {
     dispatch(selectBlock(block));
   }, [router, allBlocks, dispatch, selectedBlock]);
 
+  const handleBackClick = () => {
+    dispatch(selectBlock(null));
+    router.push(`/`);
+  };
+
   if (!selectedBlock) {
     return (
       <main className="flex justify-center pt-[72px] px-2 font-roboto-light">
@@ -52,16 +58,12 @@ export default function Block() {
     <main className="flex justify-center pt-[72px] px-2 font-roboto-light">
       <div className="flex flex-col w-[810px]">
         <div className="flex">
-          <div
-            className={`flex w-[72px] mr-6 justify-center bg-white-opacity-02 px-1 py-6 rounded-3xl shadow-lg mb-10`}
+          <button
+            className={`w-[72px] mr-6 flex justify-center items-center bg-white-opacity-02 py-6 rounded-3xl shadow-lg mb-10 hover:cursor-pointer hover:bg-white-opacity-05`}
+            onClick={handleBackClick}
           >
-            <Image
-              src="/icons/back.svg"
-              alt="Solana Icon"
-              height="25"
-              width="24"
-            />
-          </div>
+            <BackIcon height={25} width={24} fill="white" fillOpacity={0.6} />
+          </button>
           <div className="flex-grow">
             <PageHeading
               title={`Block #${selectedBlock.slot}`}
