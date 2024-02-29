d9e22648f88496c856d4e1345916ffcc358447a5
Patrick Shea <1574933+ThePatShea@users.noreply.github.com>
2024-02-29 17:47:01 -0500

    Added day counter to timestamp box.

--- helpers/getRelativeTime.ts
+++ helpers/getRelativeTime.ts
@@ -6,8 +6,13 @@ const getRelativeTime = (date: number) => {
   const seconds = now.diff(then, "seconds");
   const minutes = now.diff(then, "minutes");
   const hours = now.diff(then, "hours");
+  const days = now.diff(then, "days");
 
-  const relativeTime = `${hours}h ${minutes % 60}m ${seconds % 60}s ago`;
+  const showDays = days > 0;
+
+  const relativeTime = showDays
+    ? `${days}d ${hours % 24}h ${minutes % 60}m ${seconds % 60}s ago`
+    : `${hours}h ${minutes % 60}m ${seconds % 60}s ago`;
 
   return relativeTime;
 };
