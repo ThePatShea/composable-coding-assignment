62d73b81cba81a6f7260ebf848e95562dabbf04a
Patrick Shea <1574933+ThePatShea@users.noreply.github.com>
2024-02-29 00:17:55 -0500

    added Solana icon to block table rows

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import moment from "moment";
 
 export default function Home() {
   return (
-    <main className="flex justify-center pt-[72px] font-roboto-light">
+    <main className="flex justify-center pt-[72px] px-2 font-roboto-light">
       <div className="flex flex-col w-[800px]">
         <div className="flex p-6 rounded-3xl border border-white border-opacity-10 mb-10">
           <div className="flex justify-center mr-6 bg-black rounded-full w-14 h-14">
@@ -22,7 +22,7 @@ export default function Home() {
           placeholder="Search for transactions, blocks, accounts"
         />
         <div>
-          <div className="grid grid-cols-6 gap-4 px-6 py-4 text-white text-opacity-60">
+          <div className="grid grid-cols-6 gap-2 px-6 py-4 text-white text-opacity-60">
             <div className="col-span-1 text-xs">Block hash</div>
             <div className="col-span-1 text-xs">Slot</div>
             <div className="col-span-1 text-xs">Timestamp</div>
@@ -31,13 +31,20 @@ export default function Home() {
             <div className="col-span-1 text-xs">Reward</div>
           </div>
             {blocks.map((block, i) => (
-              <div key={i} className="grid grid-cols-6 gap-4 bg-white-opacity-02 hover:bg-white-opacity-05 px-6 py-[18px] rounded-2xl mb-1 text-white text-opacity-60 hover:text-opacity-100 hover:cursor-pointer">
+              <div key={i} className="grid grid-cols-6 gap-2 bg-white-opacity-02 hover:bg-white-opacity-05 px-6 py-[18px] rounded-2xl mb-1 text-white text-opacity-60 hover:text-opacity-100 hover:cursor-pointer">
                 <div className="col-span-1 text-sm text-picasso text-opacity-100 hover:text-picasso-opacity-50 truncate">{block.blockHash}</div>
                 <div className="col-span-1 text-sm text-picasso text-opacity-100 hover:text-picasso-opacity-50">#{block.slot}</div>
                 <div className="col-span-1 text-sm">{moment.unix(block.timestamp).fromNow()}</div>
                 <div className="col-span-1 text-sm">{block.txCount}</div>
                 <div className="col-span-1 text-sm text-picasso text-opacity-100 hover:text-picasso-opacity-50 truncate">{block.leader}</div>
-                <div className="col-span-1 text-sm">{block.rewardSol} SOL ({formatAsUsd(block.rewardUsd)})</div>
+                <div className="col-span-1 text-sm flex">
+                  <div className="flex justify-center mr-2 bg-black rounded-full w-4 h-4">
+                    <Image src="/icons/solana.svg" alt="Solana Icon" width="9" height="9"/>
+                  </div>
+                  <div>
+                    {block.rewardSol} SOL ({formatAsUsd(block.rewardUsd)})
+                  </div>
+                </div>
               </div>
             ))}
         </div>
